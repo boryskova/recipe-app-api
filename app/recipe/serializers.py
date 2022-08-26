edd8628d1be6c58edb00041702167c85b374ccb4
boryskova <borysovamo@gmail.com>
2022-08-26 18:00:54 +0300

    Add Recipe Image API

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -93,4 +93,14 @@ class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail."""
 
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
+        fields = RecipeSerializer.Meta.fields + ['description', 'image']
+
+
+class RecipeImageSeriazlizer(serializers.ModelSerializer):
+    """Serializer for uploading images to the recipe."""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'image']
+        read_only_fields = ['id']
+        extra_kwargs = {'image': {'required': True}}
